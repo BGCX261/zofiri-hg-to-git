e41f584f3605d2e15bea913d9fa0bbc63aa4a433
Thomas Palmer <nospam@tjpalmer.com>
2010-05-11 13:56:05 -0500

    More thoughts on parts.

--- test/parts.py
+++ test/parts.py
@@ -11,17 +11,46 @@ class Part(object):
     sometime, but that's not today.) Parts shouldn't have internally
     drivable motors. Rather motors should exist at joints between parts,
     but I may reconsider this style later.
-    
+
     Parts should be cloneable, alone or in chains, to allow easy building
     of composite structures.
+
     TODO Or should composite structures be built separately from
     immutable parts? Still seems like parts ought to be paramterizable
-    say for different sizes and so on.
+    say for different sizes and so on. So go with copying them for now.
     """
 
     def __init__(self):
-        # TODO Pass in a shape or let shapes subclass parts?
-        pass
+        self.id = None
+        self.joints = []
+        self.sockets = []
+
+    def add_joint(self, joint):
+        joint.part = self
+        self.joints.append(joint)
 
 class Capsule(Part):
-    pass
+
+    def __init__(self, radius, half_spread, joint_pos=0.5, joint_axis=(1,0,0)):
+        """
+        Param 'joint_pos' indicates how far down the end spheres the
+        default joint should be. The hinge is at the positive Y end.
+        A corresponding socket is placed on negative Y, with the same
+        axis and a matching position.
+        """
+        Part.__init__(self)
+        self.radius = radius
+        self.half_spread = half_spread
+        self.add_joint(Joint(half_spread + joint_pos * radius, joint_axis))
+
+class Joint(object):
+    """
+    Represents a hinge joint, but might extend this to 6-DOF joints.
+    Positions and axes are relative to the part coordinate frame.
+    """
+
+    def __init__(self, pos, axis):
+        self.next = None;
+        self.part = None
+        self.pos = pos
+        self.axis = axis
